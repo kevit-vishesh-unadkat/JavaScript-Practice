9d0b14a2bd19cece298b6d6caf7809c45c970667
kevit-vishesh-unadkat <vishesh.unadkat@kevit.io>
2026-09-14 16:43:35 +0530

    Explore Some more concept

--- mordern_js_method.js
+++ mordern_js_method.js
@@ -2,7 +2,8 @@ const products = [
   { id: 1, name: "Laptop", category: "Electronics", price: 1200, inStock: true },
   { id: 2, name: "Book", category: "Books", price: 30, inStock: true },
   { id: 3, name: "Coffee Maker", category: "Appliances", price: 150, inStock: false },
-  { id: 4, name: "Headphones", category: "Electronics", price: 200, inStock: true }
+  { id: 4, name: "Headphones", category: "Electronics", price: 200, inStock: true },
+  { id: 5, name: "Coffee Maker", category: "Appliances", price: 220, inStock: true },
 ];
 
 
@@ -50,3 +51,34 @@ const totalStockValue=products.reduce((sum,product)=>{
 },0);
 
 console.log(totalStockValue)
+
+
+
+
+//find method
+
+//find->it stops and return the vry first eleement that matches the conditions
+
+const coffeeMaker=products.find(product=>{
+    return product.name==="Coffee Maker";
+});
+
+console.log(coffeeMaker);
+
+
+//some() -> at least one element in the array passes the test it will return the true.
+
+const hasOutOfStock=products.some(product=>{
+    return product.inStock===false;
+})
+
+console.log(hasOutOfStock);
+
+
+//every -> checks if all the element in the array pass the test  , return true or false
+
+const areAllItemsINStock=products.every(product=>{
+    return product.inStock===true;
+});
+
+console.log(areAllItemsINStock);
